948cbb35cb6e14f778c3e3f0acf462cfca4d0a1f
ef <estrada.flsw@gmail.com>
2026-01-30 13:06:02 -0600

    data: update usernames of social links

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,33 +1,38 @@
 import type { Link } from "../types";
 
-export const name = '100 JS'
-export const title = '100 proyectos de JavaScript'
-export const description = ''
-export const preview = 'https://100js.esflowers.icu/preview.png'
+export const name = "100 JS";
+export const title = "100 proyectos de JavaScript";
+export const description = "";
+export const preview = "https://100js-dev.vercel.app/preview.png";
 
 export const site: Link = {
     title: name,
-    url: 'https://100js.esflowers.icu',
-    label: '100js.esflowers.icu'
-}
-
-export const ig: Link = {
-    title: 'Instagram',
-    url: 'https://instagram.com/esflowers._/'
-}
+    url: "https://100js-dev.vercel.app",
+    label: "100js-dev.vercel.app"
+};
 
 export const repository: Link = {
-    title: 'GitHub',
-    url: 'https://github.com/esflowers/100-js',
-    label: 'Repositorio'
-}
+    title: "GitHub",
+    url: "https://github.com/e-iii/100-js",
+    label: "Repositorio"
+};
+
+export const ig: Link = {
+    title: "Instagram",
+    url: "https://instagram.com/e_iii___"
+};
+
+export const x: Link = {
+    title: "X",
+    url: "https://x.com/e_iii___"
+};
 
 export const midudev: Link = {
-    title: 'Midudev',
-    url: 'https://midu.dev/'
-}
+    title: "Midudev",
+    url: "https://midu.dev/"
+};
 
 export const creator: Link = {
-    title: '100 proyectos JS',
-    url: 'https://www.javascript100.dev/'
-}
+    title: "100 proyectos JS",
+    url: "https://www.javascript100.dev/"
+};
