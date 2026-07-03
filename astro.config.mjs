da7532321b08a6ca24c165bcfb797bba7c6beeac
iii <estrada.flsw@gmail.com>
2026-07-03 12:05:52 -0600

    build: config environment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+import tailwindcss from "@tailwindcss/vite"
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  devToolbar: {
-    enabled: false
-  },
-  vite: {
-    plugins: [tailwindcss()]
-  }
-});
+    site: "https://100jsw.vercel.app",
+    devToolbar: {
+        enabled: false
+    },
+    vite: {
+        plugins: [tailwindcss()]
+    }
+})
