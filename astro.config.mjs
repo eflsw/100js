cbb379b8e84793bda289a48370a1601a53713dc3
iii <estrada.flsw@gmail.com>
2026-07-05 17:05:35 -0600

    feat: udpate fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config"
+import { defineConfig, fontProviders } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
@@ -8,5 +8,37 @@ export default defineConfig({
     },
     vite: {
         plugins: [tailwindcss()]
-    }
+    },
+    fonts: [
+        {
+            provider: fontProviders.local(),
+            name: "Onest",
+            cssVariable: "--font-sans",
+            weights: [400, 500, 600],
+            fallbacks: ["system-ui", "sans-serif"],
+            options: {
+                variants: [
+                    {
+                        src: ["./src/assets/fonts/Onest.woff2"],
+                        style: "normal"
+                    }
+                ]
+            }
+        },
+        {
+            provider: fontProviders.local(),
+            name: "Geist Mono",
+            cssVariable: "--font-mono",
+            weights: [400, 500, 600],
+            fallbacks: ["ui-monospace", "monospace"],
+            options: {
+                variants: [
+                    {
+                        src: ["./src/assets/fonts/GeistMono.woff2"],
+                        style: "normal"
+                    }
+                ]
+            }
+        }
+    ]
 })
